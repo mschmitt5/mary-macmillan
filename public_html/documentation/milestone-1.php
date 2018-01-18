22ceb9c39b16b365f68802df101e3fb4768c7eb3
Mary <schmitt.mary7@gmail.com>
2018-01-17 21:28:50 -0700

    train work: added some persona details, updated audience, added user story.

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -9,14 +9,15 @@
     <h2>Purpose</h2>
     <p>I would like my PWP to act as a resume and as proof of my web development skills.</p>
     <h2>Audience</h2>
-    <p>My main audience is potential employers and other developers.</p>
+    <p>My main audience is potential employers and other developers; my site will be designed for professionals in the web development community.</p>
     <h2>Goal</h2>
     <p>My first goal is to acquire full-time employment by using my personal website as a gallery to showcase and promote my skills.</p>
     <p>My secondary goal is to increase the reach of a new or established social media presence.</p>
     <h2>Persona</h2>
-    <p>Name</p>
-    <p>detailed description that includes age, job or role, relevant technology being used, behaviors and attitudes, and the situatin or scenario in which they will use my site.</p>
-    <p>their goals regarding the use of my site.</p>
-    <p>additional info: frustrations the persona may be facing. additional info the persona may need in order to acheive their goal. </p>
+    <p>Dominic Caldwell</p>
+    <p>Dominic is a 38-year-old man living in Santa Fe who started his own web development company - New Waves - in 2015 and is looking to hire new full-time developers. He is very experienced with technology and while browsing often uses an iPhone7. If he finds a site of particular interest he will open the link on his MacBookPro. He expects job applicants to have their own website showing their experience in back and front end development. He has a busy life due to his fairly new start up and will not spend more than 5 minutes looking at a potential employee's site. If he cannot find what he needs (resume, contact information, and portfolio) within that time he moves on.</p>
+    <p>Dominic's goals while looking at my site are to find a gallery that shows off coding experience in a way that highlights both front and back end development skills. He wants to find a skilled, young developer who will fit in with the employees at his company and seems able to learn on the job.</p>
+    <h3>User Story</h3>
+    <p>As a user, I want to learn if this developer has the required skills to work at my company as a junior developer.</p>
 </body>
 </html>
