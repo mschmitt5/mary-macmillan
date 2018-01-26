e2b31af6026e2b1713ab8793db6bddb6628e2aeb
Mary <schmitt.mary7@gmail.com>
2018-01-26 06:16:05 -0700

    added milestone-2 and added paragraphs for milestone 2a. they need work

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,15 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Mary MacMillan</title>
+</head>
+<body>
+    <h1>Milestone 2a</h1>
+    <h2>Content Strategy</h2>
+    <p>First of all, I need a section with my name and a short description of what I do. (I still haven't decided on what to focus on in terms of front or back end, but hopefully I'll have a better idea by the end of this weekend.) I will also have links to my resume, linkedin, and github. It's essentially a business card at the top of the page.</p>
+    <p>Second, I will have a more in depth "about me" section. This will have more specifics about what I'm interested in, what languages I'm comfortable with, and probably a bit about deep dive.</p>
+    <p>Third, I will have a gallery/portfolio section. Ideally I'd like it to include different projects I've worked on, but I'm worried that I won't have any real examples by that time.</p>
+</body>
+</html>
