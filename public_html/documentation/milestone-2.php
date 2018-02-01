599cee302fc0ec9c19ac214ecb087c1ec9321622
Mary <schmitt.mary7@gmail.com>
2018-02-01 13:36:53 -0700

    updated content strategy

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -10,7 +10,7 @@
     <h2>Content Strategy</h2>
     <p>First of all, I need a section with my name and a short description of what I do. (I still haven't decided on what to focus on in terms of front or back end, but hopefully I'll have a better idea by the end of this weekend.) I will also have links to my resume, linkedin, and github. It's essentially a business card at the top of the page.</p>
     <p>Second, I will have a more in depth "about me" section. This will have more specifics about what I'm interested in, what languages I'm comfortable with, and probably a bit about deep dive.</p>
-    <p>Third, I will have a gallery/portfolio section. Ideally I'd like it to include different projects I've worked on, but I'm worried that I won't have any real examples by that time.</p>
+    <p>Third, I will have a gallery/portfolio section. I've updated this to showcase only my capstone. I would like a photo of the project to be able to click on and take you to the project (no enlargement of photo, etc).</p>
     <p>I'll also have a "contact me" section. There will be fields for the person to fill in their information and email as well as a message.</p>
     <h2>Wireframes</h2>
     <img src="images/wireframe2.png" alt="wireframes">
