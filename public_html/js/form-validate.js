64013144a9a9c91cb0e575c3473a7c81c974f575
Mary <schmitt.mary7@gmail.com>
2018-02-22 10:11:21 -0700

    removed mail-config.php before adding any info. updated directory structure.

--- public_html/js/form-validate.js
+++ public_html/js/form-validate.js
@@ -14,7 +14,7 @@ $(document).ready(function(){
      **/
 
     /* begin validate function here */
-    $("#my-contact-form").validate({
+    $("#form").validate({
 
         // setup handling of form errors
         debug: true,
@@ -69,9 +69,9 @@ $(document).ready(function(){
 
         // AJAX submit the form data to back end if rules pass
         submitHandler: function(form) {
-            $("#my-contact-form").ajaxSubmit({
+            $("#form").ajaxSubmit({
                 type: "POST",
-                url: $("#my-contact-form").attr("action"),
+                url: $("#form").attr("action"),
 
                 success: function(ajaxOutput) {
                     // clear the output area's formatting
@@ -82,7 +82,7 @@ $(document).ready(function(){
 
                     // reset the form if it was successful
                     if($(".alert-success").length >= 1) {
-                        $("#my-contact-form")[0].reset();
+                        $("#form")[0].reset();
                     }
                 }
             })
